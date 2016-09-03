44508c91ff194daab9189558b152f8b8b5955abf
joelbandi <joelvivekbandi@gmail.com>
2016-09-03 06:40:26 -0700

    upgraded Btree

--- DS/Trees&Graphs/Btree.java
+++ DS/Trees&Graphs/Btree.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Stack;
 public class Btree{
     Btree left;
     Btree right;
+    Btree parent;
     int value;
     boolean isEmpty;
 
@@ -9,24 +12,70 @@ public class Btree{
         this.isEmpty=false;
         this.right=null;
         this.left=null;
+        this.parent=null;
     }
 
-    Btree(int v,Btree r,Btree l){
+    Btree(int v,Btree r,Btree l,Btree p){
         this.value=v;
+        this.isEmpty=false;
         this.right=r;
         this.left=l;
-        this.isEmpty=false;
+        this.parent=parent;
     }
 
     Btree(){
         this.right=null;
-        this.left=null; 
+        this.left=null;
+        this.parent=null; 
         this.isEmpty=true;
+    }
+
+    /**
+     * 
+     * This Function creates a tree representation from a preorder sequence arraylist of a tree.
+     * Every unique tree has its own unique pre order traversal pattern.
+     * 
+     * */
+
+
+    public static Btree create(ArrayList<Integer> list){
+        if(list == null){
+            return null;
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = list.size()-1 ; i >=0 ; i--){
+            stack.push(list.get(i));
+        }
+        return preorderadd(new Btree(),stack);
+        
+    }
+
+    private static Btree preorderadd(Btree btree, Stack<Integer> stack){
+        if(stack.empty()){
+            return null;
+        }
+        btree.setPayload(stack.pop());
+        btree.setLeft(preorderadd(new Btree(),stack));
+        btree.setRight(preorderadd(new Btree(),stack));
+        return btree;
     }
 
     public void setPayload(int v){
         this.value=v;
         this.isEmpty=false;
+    }
+
+
+    /**
+     *  API practice: optional tho
+     *  Btree one = new Btree(3);
+     *  Btree two = new Btree(5);
+     *  one.setRight(two.setParent(one));
+     **/
+
+    public Btree setParent(Btree p){
+        this.parent=p;
+        return this;
     }
 
     public void setRight(Btree r){
@@ -70,7 +119,36 @@ public class Btree{
             this.right.inorder();
         }
     }
-    
 
+    public void revinorder(){
+        if(this.right!= null){
+            this.right.inorder();
+        }
+        System.out.println(this.value);
+        if(this.left!=null){
+            this.left.inorder();
+        }
+    }
+
+    public void preorder(){
+        System.out.println(this.value);
+        if(this.right!= null){
+            this.right.preorder();
+        }
+        if(this.left!=null){
+            this.left.preorder();
+        }
+    }
+
+    public void postorder(){
+        if(this.right!= null){
+            this.right.postorder();
+        }
+        if(this.left!=null){
+            this.left.postorder();
+        }
+        System.out.println(this.value);
+    }
+    
 
 }
--- DS/Trees&Graphs/BtreeTest.java
+++ DS/Trees&Graphs/BtreeTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class BtreeTest{
     public static void main(String ...a){
         Btree b = new Btree(5);
@@ -5,6 +6,19 @@ public class BtreeTest{
         b.add(2);b.add(4);
         b.add(6);b.add(8);
         b.inorder();
+        System.out.println("-----------------");
+        b.preorder();
+
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(5);
+        al.add(7);
+        al.add(8);
+        al.add(6);
+        al.add(3);
+        al.add(4);
+        al.add(2);
+        Btree nb = Btree.create(al);
+        System.out.println("-----------------");
+        nb.preorder();
     }
-    
 }
